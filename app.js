94bf15b02db426d1031efe28d930be9b30fc28c0
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2022-01-07 15:45:48 +0100

    ALL DONE!

--- app.js
+++ app.js
@@ -27,3 +27,58 @@ fetch("./available.json")
 $(".nav-link").on("click", function () {
 	$(".navbar-collapse").collapse("hide");
 });
+
+//Animations
+
+var tl = gsap.timeline({});
+
+// Section Services Animations
+var tl1 = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#services",
+		start: "top 30%",
+	},
+});
+
+tl1.from("#services_line", 0.75, { y: 75 }).from("#services_s", 0.6, { y: 125 }, "-=0.65");
+
+// Section Workflow Animations
+var tl2 = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#workflow",
+		start: "top 30%",
+	},
+});
+
+tl2.from("#workflow_line", 0.75, { y: 75 })
+
+	.from(".workflow_f", 0.6, { y: 125 }, "-=0.65")
+	.from(".workflow_l", 0.6, { y: 100 }, "-=0.5");
+
+// Button Animation
+var tl3 = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#button",
+		start: "top 90%",
+	},
+});
+
+tl3.from("#button", 0.5, { scale: 0 });
+
+// Section Contact Animations
+var tl4 = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#contact",
+		start: "top 80%",
+	},
+});
+
+let ww = $(window).width();
+
+tl4.from("#f_title", 0.6, { y: 50, opacity: 0 })
+
+	.from("#f_text", 0.6, { y: 50, opacity: 0 }, "-=0.4")
+	.from("#f_button", 0.6, { scale: 0 }, "-=0.4")
+	.from("#icons", 0.6, { width: ww, opacity: 0 }, "-=0.35")
+	.from("#copy1", 0.6, { x: -50, opacity: 0 }, "-=0.3")
+	.from("#copy2", 0.6, { x: 50, opacity: 0 }, "-=0.6");
